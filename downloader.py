63b26503dcdaa2d88a9a5305bcf32ea21903e596
snake <827161257@qq.com>
2024-01-27 12:04:42 +0800

    增加downloader封装类

--- downloader.py
+++ downloader.py
@@ -1,4 +1,25 @@
 import yt_dlp
+from pandas import read_csv
+
+
+class Downloader:
+    def __init__(self, csv_pth, download_dir, proxy='http://127.0.0.1:10809'):
+        self.df = read_csv(csv_pth, header=None, names=['id', 'start_time', 'end_time', 'x', 'y'],
+                           index_col='id')
+
+        self.ydl_opts = {
+            'format': 'best[ext=mp4]',
+            'outtmpl': f'{download_dir}/%(id)s/%(autonumber)d.%(ext)s',
+            'ignoreerrors': True,
+            'download_ranges': lambda info, _: self.df.loc[[info['id']], ['start_time', 'end_time']].to_dict('records'),
+            'proxy': proxy
+        }
+
+    def download(self):
+        grouped = self.df.groupby('id')
+        for vid, group in grouped:
+            with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+                ydl.download([f'https://www.youtube.com/watch?v={vid}'])
 
 
 def my_hook(d):
@@ -39,4 +60,3 @@ def download(video_id, video_result_path, audio_result_path):
 
     with yt_dlp.YoutubeDL(get_ydl_opts_aud(audio_result_path)) as ydl:
         ydl.download(['https://www.youtube.com/watch?v=' + video_id])
-
