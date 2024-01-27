9515e0e66375e4e87f6b532ed5a9c7d3245cc824
snake <827161257@qq.com>
2024-01-27 12:01:55 +0800

    用lambda表达式表示时间段

--- test/download.py
+++ test/download.py
@@ -15,25 +15,14 @@ if __name__ == '__main__':
     df = read_csv('../avspeech_train.csv', header=None, names=['id', 'start_time', 'end_time', 'x', 'y'],
                   index_col='id')
 
-
-    def download_ranges_callback(info, ydl):
-        vid = info['id']
-
-        # [vid]避免单行数据被转换为Series
-        return df.loc[[vid]].apply(lambda row: {
-            'start_time': row['start_time'],
-            'end_time': row['end_time']
-        }, axis=1).tolist()
-
-
-    data_dir = '../data/train'
+    data_dir = '.'
 
     ydl_opts = {
         'format': 'best[ext=mp4]',
         'outtmpl': f'{data_dir}/%(id)s/%(autonumber)d.%(ext)s',
         # 'simulate': True,
         'ignoreerrors': True,
-        'download_ranges': download_ranges_callback,
+        'download_ranges': lambda info, _: df.loc[[info['id']], ['start_time', 'end_time']].to_dict('records'),
         'proxy': 'http://127.0.0.1:10809'
     }
 
